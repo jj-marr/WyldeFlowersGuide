fb2d92d21f5226c936a6ad3cc38743afcdc436d9
JJ Marr <jjmarr@gmail.com>
2025-03-31 16:02:28 -0400

    Add navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,26 @@
 <script lang="ts">
 	import '../app.css';
+	import { Navigation } from '@skeletonlabs/skeleton-svelte';
+	// Icons
+	import IconHome from '@lucide/svelte/icons/home';
+	import IconFood from '@lucide/svelte/icons/utensils';
 
+	// State
+	let value = $state('files');
 	let { children } = $props();
-</script>
+  </script>
 
-{@render children()}
+<div class="card border-surface-100-900 grid h-screen w-full grid-cols-[auto_1fr] border-[1px]">
+  <!-- Navigation Rail -->
+  <Navigation.Rail {value} onValueChange={(newValue) => (value = newValue)}>
+    {#snippet tiles()}
+      <Navigation.Tile href="/" label="Home"><IconHome /></Navigation.Tile>
+      <Navigation.Tile href="/food" label="Foods"><IconFood /></Navigation.Tile>
+    {/snippet}
+  </Navigation.Rail>
+
+  <!-- Main Content -->
+  <div class="overflow-auto">
+    {@render children()}
+  </div>
+</div>
